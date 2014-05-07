000e2d3ea2ff5f6d5160291d4e4811bd2e38cb01
fajka <fajka@hyperreal.info>
2014-05-07 18:00:01 +0200

    logged in - different template, ugly version

--- src/Hyperreal/AcaciaBundle/Controller/DefaultController.php
+++ src/Hyperreal/AcaciaBundle/Controller/DefaultController.php
@@ -5,19 +5,23 @@ namespace Hyperreal\AcaciaBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="_welcome")
      * @Method("GET")
-     * @Template()
      */
     public function indexAction()
     {
-        return array(
-            'name' => 'Acacia',
-        );
+        if ($this->getUser() == null) {
+            return $this->render('HyperrealAcaciaBundle:Default:index.html.twig', array(
+                'name' => "Acacia"
+            ));
+        } else {
+            return $this->render('HyperrealAcaciaBundle:Default:indexLoggedIn.html.twig', array(
+                'name' => 'Logged in Acacia',
+            ));
+        }
     }
 }
