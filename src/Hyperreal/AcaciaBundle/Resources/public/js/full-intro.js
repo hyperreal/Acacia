1e70433abaef8e777236394d24de7f1b9a5deae4
Redakcja Hyperreala <git@hyperreal.biz>
2014-05-08 22:56:37 +0700

    full screen intro.

--- src/Hyperreal/AcaciaBundle/Resources/public/js/full-intro.js
+++ src/Hyperreal/AcaciaBundle/Resources/public/js/full-intro.js
@@ -1,4 +1,7 @@
-$('.carousel').carousel({
-        interval: 9999 //changes the speed
-	pause: "false"
+var $ = jQuery.noConflict();
+$(document).ready(function() {
+	$('.carousel').carousel({
+        	interval: 9999 //changes the speed
+		pause: "false"
 })
+});
